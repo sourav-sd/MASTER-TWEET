25e3b68aef8d68018c180431ffb9730fa9b29171
swatiwithdelgence <swati.delgence@gmail.com>
2017-05-25 14:58:20 +0530

    25th May 1st Push...

--- server.js
+++ server.js
@@ -12,24 +12,64 @@ db.once('open', function() {
     console.log('MongoDB Successfully Connected!!');
 });
 
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('views'));
 
- var personSchema = mongoose.Schema({
-     name: String,
-     address: String,
-     email: String,
-     password: String
- });
+var personSchema = mongoose.Schema({
+    name: String,
+    address: String,
+    email: String,
+    password: String
+});
 
- var Person = mongoose.model("Person", personSchema);
+var Person = mongoose.model("Person", personSchema);
+
+
+app.post('/register', function(req,res){
+    console.log(req.body);
+    var newPerson = new Person({
+        name: req.body.name,
+        address: req.body.address,
+        email: req.body.email,
+        password: req.body.pwd
+    });
+    newPerson.save(function(err, Person){
+        if(err)
+            res.send("Error please check again!!");
+        else
+            res.send(Person);
+        console.log('Registered!');
+    });
+
+});
+
+app.post('/login', function(req,res){
+    console.log(req.body);
+    Person.findOne(req.body, function(err, Person){
+        if(err)
+            res.status(500).send();
+        else
+        if(Person == null){
+            console.log('not found');
+            res.status(404).send();
+        }
+        else{
+            console.log(Person);
+            res.send(Person);
+            console.log('Logged In!');
+        }
+
+    })
+
+});
+
+
+
 
 app.get('/user', function(req, res){
-    res.sendFile(path.join(__dirname+ '/views' + '/home.html'));
+   // res.sendFile(path.join(__dirname+ '/views' + '/home.html'));
 });
 
 app.post('/user', function(req, res){
@@ -70,7 +110,7 @@ app.post('/user', function(req, res){
                     res.status(500).send();
                 else
                     console.log(Person);
-                    //res.send(Person);
+                //res.send(Person);
             })
             console.log(personInfo);
             console.log('Login!');
