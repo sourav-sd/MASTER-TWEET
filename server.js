3928fb1015b32f4493573024d5530c6d11102ee6
swatiwithdelgence <swati.delgence@gmail.com>
2017-06-02 16:56:49 +0530

    New Push PM system...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var crypto = require('crypto');
 var app = express();
 
 var mongoose = require('mongoose');
@@ -18,29 +19,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var personSchema = mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true
+    },
     address: String,
     email: {
         type: String,
         unique: true,
         required: true
     },
-    role : String,
-    password: String
+    role : {
+        type: String,
+        required: true
+    },
+    hash: String,
+    salt: String
 });
+
+personSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+personSchema.methods.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    return this.hash === hash;
+};
 
 var Person = mongoose.model("Users", personSchema);
 
 
 app.post('/register', function(req,res){
+
     console.log(req.body);
     var newPerson = new Person({
         name: req.body.name,
         address: req.body.address,
         email: req.body.email,
-        role : req.body.role,
-        password: req.body.password
+        role : req.body.role
     });
+    var cryptPass = newPerson.setPassword(req.body.password);
+    console.log(cryptPass);
     newPerson.save(function(err, Person){
         if(err)
             res.status(500).send();
@@ -54,7 +74,7 @@ app.post('/register', function(req,res){
 
 app.post('/login', function(req,res){
     console.log(req.body);
-    Person.findOne(req.body, function(err, Person){
+    Person.findOne({email: req.body.email}, function(err, Person){
         if(err)
             res.status(500).send();
         else
@@ -63,9 +83,15 @@ app.post('/login', function(req,res){
             res.status(404).send();
         }
         else{
-            console.log(Person);
-            res.send(Person);
-            console.log('Logged In!');
+
+            if (!Person.validPassword(req.body.password)) {
+                console.log('wrong pass');
+            }
+           else{
+                console.log(Person);
+                res.send(Person);
+                console.log('Logged In!');
+            }
         }
     })
 });
